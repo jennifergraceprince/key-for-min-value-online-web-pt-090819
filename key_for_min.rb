c4b3c99ca49dcbfbe49ebd71ad24d2e7c66125a8
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-27 06:04:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,4 +4,11 @@ def key_for_min_value(hash)
   lowest_item = nil
   lowest_count = nil
   hash.each do |key, value|
+    if lowest_count == nil || value < lowest_count
+      lowest_count = value
+      lowest_item = key
+    end
+  end
+  lowest_item
+end
 
