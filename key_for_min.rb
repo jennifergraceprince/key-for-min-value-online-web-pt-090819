fffbb3189a233378ce1e9b4495a7eea333f53304
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-27 06:02:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,4 @@
 clothing = {jacket: 1, pant: 2, top: 5}
 
-def key_for_min_value(hash)
-  min_item = nil
-  min_amount = nil
-  hash.each do |item, amount|
-    if min_amount == nil || amount < min_amount
-      min_item = item
-      min_amount = count
-    end
-  end
-min_item
+
+
