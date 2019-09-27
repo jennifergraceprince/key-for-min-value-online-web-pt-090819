bd63211e4edd9ca17aee55586bd519ba3330ba60
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-27 05:44:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  min_value = nil
+  min_amount = nil
+  
 end
